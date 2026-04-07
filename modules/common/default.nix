aace8de879c0fd0b4dc20f074b47a44faf7ec7b0
Daniel Lowry <development@daniellowry.co.uk>
2026-04-08 00:39:53 +0100

    🛂 Replace sudo with doas

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -13,6 +13,17 @@
 
   hardware.cpu.amd.updateMicrocode = true;
 
+  security.sudo.enable = false;
+
+  security.doas = {
+    enable = true;
+    extraRules = [{
+      groups   = [ "wheel" ];
+      keepEnv  = true;
+      persist  = true;
+    }];
+  };
+
   users.users.dandyrow = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
